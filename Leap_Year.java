85610f9d2c0d01e5528441de9e642cefd2467812
Sumedh Nimbalkar <126437154+nimbalkarsumedh@users.noreply.github.com>
2023-03-24 13:39:30 +0530

    Add files via upload

---
+++ Leap_Year.java
@@ -0,0 +1,21 @@
+package Online_Batch_Assignment;
+
+import java.util.Scanner;
+
+public class Leap_Year
+{
+    public static void main(String[] args) 
+    {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Year");
+		int year=sc.nextInt();
+		if(year%4==0)
+		{
+			System.out.println("It is a leap year");
+		}
+		else
+		{
+			System.out.println("It is not a leap year");
+		}
+	}
+}
